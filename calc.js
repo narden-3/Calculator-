e303a87964432d235e87fdff0b7a7e94dc5292d8
Narden_Nashat <82228934+narden-3@users.noreply.github.com>
2023-05-14 11:25:04 +0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,18 @@
+name1 = "A", name2 = "B", name3 = "C", name4 = "D", name5 = "E"
+result = prompt("write").toUpperCase()
+getElementById(write("Write").toUpperCase())
+if (result == name1) {
+  document.write("You Will Get Sale 10%");
+}
+else if (result == name2) {
+  document.write("You Will Get Sale 20%")
+}
+else if (result == name3) {
+  document.write("You Will Get Sale 30%")
+}
+else if (result == name4) {
+  document.write("You Will Get Sale 40%")
+}
+else if (result == name5) {
+  document.write("You Will Get Sale 50%")
+}
